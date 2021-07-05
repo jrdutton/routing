31dcbdd3d16ec9e123956dce904c9266e0804eeb
James Dutton <jdutton@rdt.co.uk>
2021-07-05 10:17:34 +0100

    initial commit

---
+++ src/app/layout1/layout1.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-layout1',
+  template: `
+    <p>
+      layout1 works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class Layout1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
