18bc8ef5d2dd95a572d08dfa3eee593345a36c91
James Dutton <jdutton@rdt.co.uk>
2021-07-05 11:47:26 +0100

    JRD

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,22 @@ import { Component } from "@angular/core";
       <a [routerLink]="['feature1']">Feature 1</a>
     </div>
     <div>
-      <a [routerLink]="['feature1', '123456']">Feature 1 With Id</a>
+      <a [routerLink]="['feature1', '123456']"
+        >Feature 1 With Id And No Menu Outlet</a
+      >
+    </div>
+    <div>
+      <a
+        [routerLink]="[
+          {
+            outlets: {
+              primary: ['feature1', '123456'],
+              menu: ['feature1', '123456']
+            }
+          }
+        ]"
+        >Feature 1 With Id And Menu Outlet</a
+      >
     </div>
     <div>
       <a [routerLink]="['feature2']">Feature 2</a>
